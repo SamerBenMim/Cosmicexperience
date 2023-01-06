64bc03b15a41861fb05b2b107e2aa4a8713176bb
BorchaniSalma <sborcheni@ieee.org>
2023-01-06 22:29:57 +0100

    registration form

--- cosmicexperience-frontend/src/app/pages/register/register.component.ts
+++ cosmicexperience-frontend/src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -17,11 +18,42 @@ export class RegisterComponent implements OnInit {
   errorsFromDb: any = null;
   usernameError: any = null;
 
-  constructor( private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   register(): void {
-  
+    // register the user to the database if the data is correct
+    this.authService
+      .register(
+        this.username,
+        this.email,
+        this.password,
+        this.firstName,
+        this.lastName,
+        this.dateOfBirthday
+      )
+      .subscribe(
+        (result) => {
+          console.log(result);
+          this.usernameError = null;
+          this.errorsFromDb = null;
+          const link = ['login'];
+          this.router.navigate(link);
+        },
+        (error) => {
+          const message = error.error.message;
+          if (Array.isArray(message)) {
+            this.errorsFromDb = message;
+            this.usernameError = null;
+          } else {
+            this.usernameError = message;
+            this.errorsFromDb = null;
+          }
+        },
+        () => {
+          console.log('register completed');
+        }
+      );
   }
 }
