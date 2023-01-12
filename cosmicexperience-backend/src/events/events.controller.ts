cccc4b4271d3cab5e5e88a25e8410691bfac3b81
samer <samerbenmim@insat.u-carthage.tn>
2023-01-12 22:05:48 +0100

    events completed

--- cosmicexperience-backend/src/events/events.controller.ts
+++ cosmicexperience-backend/src/events/events.controller.ts
@@ -10,8 +10,9 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/users/auth/get-user.decorator';
+import { JwtAuthGuard } from 'src/users/auth/jwt-auth.guard';
 import { User } from 'src/users/user/entities/user.entity';
-
 import { EventService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -19,34 +20,9 @@ import { Event } from './entities/event.entity';
 
 @Controller('cycling')
 export class EventController {
-  constructor(private readonly eventService: EventService) { }
+  constructor(private readonly eventService: EventService) {}
 
-
-
-  @Get(':id')
-  findEventById(@Param('id', ParseIntPipe) id: number): Promise<Event> {
-    return this.eventService.findEventById(id);
-  }
-  @Delete(':id')
-  // @UseGuards(JwtAuthGuard)
-  deleteEventById(@Param('id', ParseIntPipe) id: number) {
-    return this.eventService.deleteEventById(id);
-  }
-  @Patch(':id')
-  // @UseGuards(JwtAuthGuard)
-  updateEventById(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateEventDto: UpdateEventDto,
-    // @GetUser() user: User,
-  ): Promise<Event> {
-    return this.eventService.updateEventById(id, updateEventDto, user);
-  }
-  @Get('user')
-  // @UseGuards(AuthGuard())
-  getEventsForUser(@GetUser() user: User): Promise<Event[]> {
-    return this.eventService.getEventsForUser(user);
-  }
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post('joinEvent/:id')
   async joinEvent(
     @GetUser() user: User,
@@ -68,5 +44,41 @@ export class EventController {
   @Get('upComingEvents')
   async getUpcomingEvents(): Promise<Event[]> {
     return this.eventService.getUpcomingEvents();
+  }
+
+  @Get('user')
+  @UseGuards(AuthGuard())
+  getEventsForUser(@GetUser() user: User): Promise<Event[]> {
+    return this.eventService.getEventsForUser(user);
+  }
+
+  @UseGuards(JwtAuthGuard) //to force auth to create events
+  @Post()
+  createEvent(
+    @Body() createEventDto: CreateEventDto,
+    @GetUser() user: User,
+  ): Promise<Event> {
+    return this.eventService.createEvent(createEventDto, user);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  updateEventById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEventDto: UpdateEventDto,
+    @GetUser() user: User,
+  ): Promise<Event> {
+    return this.eventService.updateEventById(id, updateEventDto, user);
+  }
+
+  @Delete(':id')
+  // @UseGuards(JwtAuthGuard)
+  deleteEventById(@Param('id', ParseIntPipe) id: number) {
+    return this.eventService.deleteEventById(id);
+  }
+
+  @Get(':id')
+  findEventById(@Param('id', ParseIntPipe) id: number): Promise<Event> {
+    return this.eventService.findEventById(id);
   }
 }
