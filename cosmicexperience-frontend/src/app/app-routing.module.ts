16bf19b018638ea639b91380ef3b3d73038063e1
samer <samerbenmim@insat.u-carthage.tn>
2023-01-15 13:04:51 +0100

    add routing

--- cosmicexperience-frontend/src/app/app-routing.module.ts
+++ cosmicexperience-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TestComponent } from './pages/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent    },
+  {
+    path: 'register',
+    component: TestComponent,
+  },
+  { path: 'profile', component: TestComponent },
+  {
+    path: 'editProfile',
+    component: TestComponent,
+
+  },
+  {
+    path: 'createEvent',
+    component: TestComponent,
+
+  },
+  { path: 'events', component: TestComponent },
+  { path: '', component: TestComponent },
+  { path: '**', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
