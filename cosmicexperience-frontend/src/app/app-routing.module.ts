f66dfd04114d48b99060721369a660141a8d26b2
BorchaniSalma <sborcheni@ieee.org>
2023-01-16 22:08:29 +0100

    routing

--- cosmicexperience-frontend/src/app/app-routing.module.ts
+++ cosmicexperience-frontend/src/app/app-routing.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { LoginGuard } from './guards/login.guard';
+import { LogoutGuard } from './guards/logout.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 import { TestComponent } from './pages/test/test.component';
+import { CreateEventComponent } from './profile/create-event/create-event.component';
+import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
+import { ProfileComponent } from './profile/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent    },
+  { path: 'login', component: LoginComponent, canActivate: [LogoutGuard] },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [LogoutGuard],
   },
-  { path: 'profile', component: TestComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [LoginGuard] },
   {
     path: 'editProfile',
-    component: TestComponent,
-
+    component: EditProfileComponent,
+    canActivate: [LoginGuard],
   },
   {
     path: 'createEvent',
-    component: TestComponent,
-
+    component: CreateEventComponent,
+    canActivate: [LoginGuard],
   },
   { path: 'events', component: TestComponent },
   { path: '', component: HomeComponent },
