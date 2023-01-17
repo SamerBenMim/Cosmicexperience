19f33aa93eae7c793ab8af68300aa6df0a9ffa08
BorchaniSalma <sborcheni@ieee.org>
2023-01-17 19:22:51 +0100

    add css to filter*

--- cosmicexperience-backend/src/events/entities/event.entity.ts
+++ cosmicexperience-backend/src/events/entities/event.entity.ts
@@ -19,19 +19,19 @@ export class Event {
   @Column()
   description: string;
 
-  @Column( )
+  @Column()
   place: string;
 
-  @Column( )
+  @Column()
   date: Date;
 
-  @Column( { default: 1 })
+  @Column({ default: 1 })
   numberOfDays: number; 
 
-  @Column( { default: 0 })
+  @Column({ default: 0 })
   numberMaxOfParticipants: number;
 
-  @Column( { default: 0 })
+  @Column({ default: 0 })
   numberOfParticipants: number;
 
   @Column()
